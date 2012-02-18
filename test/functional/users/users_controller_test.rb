7dcd7543a616231a1e774b98eff06571af53ed07
david <david@baoleihang.com>
2012-02-18 23:57:26 +0800

    add users_controller test

--- test/functional/users/users_controller_test.rb
+++ test/functional/users/users_controller_test.rb
@@ -22,4 +22,31 @@ class Users::UsersControllerTest < ActionController::TestCase
     #login is still empty
     #assert_equal session[:user_id], new_user.id , "Session user id is different with register user id"    
   end
+
+  test "register failed, name is empty" do
+    assert_registration_failed :name => ''
+  end
+
+  test "register failed, password is empty" do
+    assert_registration_failed :password => '', :password_confirmation => ''
+    assert_registration_failed :password => ''
+    assert_registration_failed :password_confirmation => ''
+  end
+
+  test "register failed, password length is wrong" do
+    shor_password = '012'
+    assert_registration_failed :password => shor_password, :password_confirmation => shor_password
+    long_password = '0123456789012345678901234567890123456789'
+    assert_registration_failed :password => long_password, :password_confirmation => long_password
+  end
+
+  test "register failed, password not match confirmation" do
+    assert_registration_failed :password => '12345678', :password_confirmation => '123456789'
+  end
+
+  def assert_registration_failed(user)
+    assert_no_difference "User.count", "register should be failed" do
+      post :create, :user => @roshan.merge(user)
+    end
+  end
 end
