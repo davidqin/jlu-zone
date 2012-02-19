eea2c96d596d041aae480fce06eaf3598209e874
david <david@baoleihang.com>
2012-02-19 17:17:53 +0800

    add cancan controller authourize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   attr_accessor :current_user
 
   helper_method :current_user
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render "/permission_error"
+  end
 end
