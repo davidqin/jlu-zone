deffe7690ae3a5af67fe84a564553ca270004848
david <david@baoleihang.com>
2012-02-25 14:02:59 +0800

    add  index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Wiki::Application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users
+
+  root :to => 'wiki#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +55,7 @@ Wiki::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'welcome#index
 
   # See how all your routes lay out with "rake routes"
 
