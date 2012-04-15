109d26f0c594325a9e4b6a59a91785f662f42224
david <david@baoleihang.com>
2012-04-15 23:13:52 +0800

    about

--- app/controllers/community/tags_controller.rb
+++ app/controllers/community/tags_controller.rb
@@ -6,6 +6,7 @@ class Community::TagsController < ApplicationController
   def index
     @tag = Tag.find_by_number(params[:tag_id])
     @topics = @tag.topics.order("updated_at desc").all.paginate(:page => params[:page], :per_page => 20)
+    @page_title = itext("topic.tag", :tag_name => @tag.name)
     drop_breadcrumb(itext("navigation.community"), community_path)
     drop_breadcrumb(itext("topic.tag", :tag_name => @tag.name))
     render "community/index"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,12 @@ class HomeController < ApplicationController
   include Wiki::Controllers::TabsHighLight::Base
   include Wiki::Controllers::TabsHighLight::Home
   def index
-    @topics = Topic.order("updated_at desc").limit(10)
-    render "/index"
+    @page_title = "Home"
+    do_not_use_sidebar
+  end
+
+  def about
+    do_not_use_sidebar
+    @page_title = itext("navigation.about_me")
   end
 end
