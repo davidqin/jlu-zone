8b9443091b1dbb8cca8a31e0f733a8d456d393db
david <davidqhr@gmail.com>
2013-08-07 09:20:58 +0800

    email address hexdigest

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,9 +1,15 @@
+require 'digest'
+require 'yaml'
+
+USER_START_EMAIL_SALT = YAML.load_file('config/config.yml').user_start_email_salt
+
 class StartController < ApplicationController
   def form
   end
 
   def send_email
     @email_address = params[:email]
+    @email_token = Digest::SHA256.hexdigest(@email_address + USER_START_EMAIL_SALT)
     render "sent_email"
   end
 end
